010d05df84a2dc701a872c1e156d08ebddf6e590
mccord6502 <49416244+mccord6502@users.noreply.github.com>
2025-09-09 10:16:31 -0500

    Update streamlit_app.py
    Snowflake lab

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -9,7 +8,8 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
 )
-session = get_active_session()
+cnx = st.connect("snowflake")
+session = cnx.session()
 
 option = st.selectbox(
     "How would you like to be contacted?",
